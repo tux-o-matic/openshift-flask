2b0fc3c373131063562bbdd85b690cfe30ce2b1b
Benjamin Merot <ben@busyasabee.org>
2016-08-24 15:38:28 +0200

    Added base Swagger structure

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask.ext.restful import Api, Resource, fields, marshal_with
+from flask_restful_swagger import swagger
 import socket
 
 application = Flask(__name__)
+
+api = swagger.docs(Api(application), apiVersion='0.1',
+                   basePath='http://localhost:8080',
+                   resourcePath='/',
+                   produces=["application/json", "text/html"],
+                   api_spec_url='/api/spec', description='API Spec')
 
 @application.route("/")
 def hello():
