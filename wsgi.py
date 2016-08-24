d2e8ffe9c1ae49842da4ab44e4f2a1c159c9d069
Benjamin Merot <ben@busyasabee.org>
2016-08-24 14:50:40 +0200

    Added status endpoint to serve as liveness check

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,10 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     return "Hello World!"
+    
+@application.route("/status")
+def hello():
+    return "I'm alive"
 
 if __name__ == "__main__":
     application.run()
