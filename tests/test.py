2d371bb2a639aaa38e9dc6ffeb0c65250c1960ec
Benjamin Merot <ben@busyasabee.org>
2016-08-25 11:41:16 +0200

    Added shell test class for unit testing

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+import pylint
+import unittest
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
